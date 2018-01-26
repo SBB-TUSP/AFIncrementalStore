a6be7a8186515ff24c02c256b9818d5d2decc907
Alessandro Ranaldi <alessandro.ranaldi@finconsgroup.com>
2018-01-26 12:36:56 +0100

    added 2 new tests for AFIncrementalStore class

--- Examples/Basic Example/AFIncrementalStoreTests/AFIncrementalStoreTests.swift
+++ Examples/Basic Example/AFIncrementalStoreTests/AFIncrementalStoreTests.swift
@@ -148,7 +148,7 @@ class AFIncrementalStoreTests: XCTestCase {
         wait(for: [testFinishExpectation], timeout: 1)
     }
 
-    func test_executeFetchRequestShouldReturnEmptyArrayAndEmptyDB_whenDBNotEmptyAndResponseEmpty() {
+    func test_executeFetchRequestShouldReturnEmptyArray_whenDBNotEmptyAndResponseEmpty() {
         class FakeClientSubclass: FakeClient {
 
             override func request(for fetchRequest: NSFetchRequest<NSFetchRequestResult>!, with context: NSManagedObjectContext!) -> NSMutableURLRequest! {
@@ -198,6 +198,143 @@ class AFIncrementalStoreTests: XCTestCase {
         }
         XCTAssertNotNil(results)
         XCTAssertTrue(results.isEmpty)
+        wait(for: [testFinishExpectation], timeout: 1)
+    }
+
+    func test_executeFetchRequestShouldReturnNonEmptyArray_whenDBEmptyAndResponseNotEmpty() {
+        class FakeClientSubclass: FakeClient {
+
+            override func resourceIdentifier(forRepresentation representation: [AnyHashable : Any]!, ofEntity entity: NSEntityDescription!, from response: HTTPURLResponse!) -> String! {
+                return "TEST-ID"
+            }
+
+            override func attributes(forRepresentation representation: [AnyHashable : Any]!, ofEntity entity: NSEntityDescription!, from response: HTTPURLResponse!) -> [AnyHashable : Any]! {
+                let dictionary: [String: Any] = [
+                    "artistDescription": "TEST-DESCRIPTION",
+                    "name": "TEST-ARTIST",
+                    "songs": [Any]()
+                ]
+                return dictionary
+            }
+
+            override func request(for fetchRequest: NSFetchRequest<NSFetchRequestResult>!, with context: NSManagedObjectContext!) -> NSMutableURLRequest! {
+                return NSMutableURLRequest(url: URL(string: "http://localhost")!)
+            }
+
+            var successClosure: ((AFHTTPRequestOperation?, Any?) -> Void)!
+
+            var testFinishedClosure: (() -> Void)!
+
+            override func httpRequestOperation(with urlRequest: URLRequest!, success: ((AFHTTPRequestOperation?, Any?) -> Void)!, failure: ((AFHTTPRequestOperation?, Error?) -> Void)!) -> AFHTTPRequestOperation! {
+                successClosure = success
+                return AFHTTPRequestOperation(request: urlRequest)
+            }
+
+            override func representationOrArrayOfRepresentations(ofEntity entity: NSEntityDescription!, fromResponseObject responseObject: Any!) -> Any! {
+                testFinishedClosure()
+                let dictionary: [String: Any] = [
+                    "artistDescription": "TEST-DESCRIPTION",
+                    "name": "TEST-ARTIST",
+                    "songs": [[String: Any]]()
+                ]
+                return dictionary
+            }
+
+            override func enqueue(_ operation: AFHTTPRequestOperation!) {
+                successClosure(operation, [String: Any]())
+            }
+
+        }
+        let testFinishExpectation = expectation(description: "shouldFinishExecutingTest")
+        let fakeClient = FakeClientSubclass(baseURL: URL(string: "http://localhost"))
+        fakeClient?.testFinishedClosure = {
+            testFinishExpectation.fulfill()
+        }
+        store.httpClient = fakeClient
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = coordinator
+        let request = NSFetchRequest<Artist>()
+        request.entity = NSEntityDescription.entity(forEntityName: "Artist", in: context)
+        var results: [NSManagedObject]!
+        context.performAndWait {
+            results = try! self.store.execute(request, with: context) as! [NSManagedObject]
+        }
+        XCTAssertNotNil(results)
+        XCTAssertFalse(results.isEmpty)
+        let rawArtist: NSManagedObject! = results.first
+        XCTAssertNotNil(rawArtist)
+        XCTAssertEqual(rawArtist.entity.name, "Artist")
+        XCTAssertEqual(rawArtist.value(forKey: "name") as? String, "TEST-ARTIST")
+        wait(for: [testFinishExpectation], timeout: 1)
+    }
+
+    func test_executeFetchRequestShouldReturnIDs_whenRequestTypeIsIDResultType() {
+        class FakeClientSubclass: FakeClient {
+
+            override func resourceIdentifier(forRepresentation representation: [AnyHashable : Any]!, ofEntity entity: NSEntityDescription!, from response: HTTPURLResponse!) -> String! {
+                return "TEST-ID"
+            }
+
+            override func attributes(forRepresentation representation: [AnyHashable : Any]!, ofEntity entity: NSEntityDescription!, from response: HTTPURLResponse!) -> [AnyHashable : Any]! {
+                let dictionary: [String: Any] = [
+                    "artistDescription": "TEST-DESCRIPTION",
+                    "name": "TEST-ARTIST",
+                    "songs": [Any]()
+                ]
+                return dictionary
+            }
+
+            override func request(for fetchRequest: NSFetchRequest<NSFetchRequestResult>!, with context: NSManagedObjectContext!) -> NSMutableURLRequest! {
+                return NSMutableURLRequest(url: URL(string: "http://localhost")!)
+            }
+
+            var successClosure: ((AFHTTPRequestOperation?, Any?) -> Void)!
+
+            var testFinishedClosure: (() -> Void)!
+
+            override func httpRequestOperation(with urlRequest: URLRequest!, success: ((AFHTTPRequestOperation?, Any?) -> Void)!, failure: ((AFHTTPRequestOperation?, Error?) -> Void)!) -> AFHTTPRequestOperation! {
+                successClosure = success
+                return AFHTTPRequestOperation(request: urlRequest)
+            }
+
+            override func representationOrArrayOfRepresentations(ofEntity entity: NSEntityDescription!, fromResponseObject responseObject: Any!) -> Any! {
+                testFinishedClosure()
+                let dictionary: [String: Any] = [
+                    "artistDescription": "TEST-DESCRIPTION",
+                    "name": "TEST-ARTIST",
+                    "songs": [[String: Any]]()
+                ]
+                return dictionary
+            }
+
+            override func enqueue(_ operation: AFHTTPRequestOperation!) {
+                successClosure(operation, [String: Any]())
+            }
+
+        }
+        let testFinishExpectation = expectation(description: "shouldFinishExecutingTest")
+        let fakeClient = FakeClientSubclass(baseURL: URL(string: "http://localhost"))
+        fakeClient?.testFinishedClosure = {
+            testFinishExpectation.fulfill()
+        }
+        store.httpClient = fakeClient
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = coordinator
+        let request = NSFetchRequest<Artist>()
+        request.entity = NSEntityDescription.entity(forEntityName: "Artist", in: context)
+        request.resultType = NSFetchRequestResultType.managedObjectIDResultType
+        var rawResults: Any!
+        context.performAndWait {
+            _ = Artist(entity: NSEntityDescription.entity(forEntityName: "Artist", in: context)!, insertInto: context)
+            _ = try! context.save()
+            rawResults = try! self.store.execute(request, with: context)
+        }
+        XCTAssertNotNil(rawResults)
+        let arrayResults: [NSManagedObjectID]! = rawResults as? [NSManagedObjectID]
+        XCTAssertNotNil(arrayResults)
+        let id: NSManagedObjectID! = arrayResults.first
+        XCTAssertNotNil(id)
+        XCTAssert(id.uriRepresentation().lastPathComponent.contains("TEST-ID"))
         wait(for: [testFinishExpectation], timeout: 1)
     }
     
