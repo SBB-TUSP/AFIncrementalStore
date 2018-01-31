f32f53dd7e10b55b0dda98f5969a6961e8beff42
Ignazio Altomare <ignazio.altomare@finconsgroup.com>
2018-01-31 14:36:06 +0100

    created AFRESTClientSwift

---
+++ AFIncrementalStore/AFRESTClientSwift.swift
@@ -0,0 +1,15 @@
+//
+//  AFRESTClientSwift.swift
+//  
+//
+//  Created by Ignazio Altomare on 31/01/2018.
+//
+
+import UIKit
+
+class AFRESTClientSwift: AFHTTPClient {
+
+    var paginator: AFPaginator
+
+
+}
